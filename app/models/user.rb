28bb18d999b096301074e6bee8ee00c7c0a478de
Bowen Xu <jimmyxbw@gmail.com>
2019-01-06 22:03:48 -0500

    complete user signup module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, uniqueness: {case_sensitive: false},
-                    length: { minimum: 3, maximum: 25 }
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\Z/
+  validates :name, presence: {message: "用户名不填你注册个什么啊?"},
+                  length: { maximum: 40, too_long: "%{count}个字符是允许的最大值，你有这么长么?" },
+                  length: { minimum: 4, too_short: "%{count}个字符是允许的最小值，你想这么短么?" },
+                  format: { with: VALID_USERNAME_REGEX, message: "目前只允许用英文字母和数字注册"},
+                  uniqueness: {case_sensitive: false}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: {case_sensitive: false},
-                    length: { minimum: 8, maximum: 105},
-                    format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: { message: "电子邮件也要填，可以乱填一个假的电子邮件"},
+  length: { maximum: 40, too_long: "%{count}个字符是允许的最大值，这么长你记得住么？" },
+  length: { minimum: 8, too_short: "%{count}个字符是允许的最小值，你怎么这么短?" },
+  format: { with: VALID_EMAIL_REGEX, message: "电子邮件可以是假的，但是格式要正确！"},
+  uniqueness: { case_sensitive: false }
 
   has_secure_password
+  validates :password, presence: {message: "要填6位或以上的密码，比如123456 :)"},
+                        length: { minimum: 6, too_short: "密码太短了，最少也得%{count}位吧?" }
+
+  validates :password_confirmation, presence: {message: "确认密码要填6位或以上的密码，并且要和上面相同"}
 end
